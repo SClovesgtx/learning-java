de02d6627f83d72e6f773c1c497eb60b3db42728
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-01-14 15:54:22 -0300

    prints e variáveis

---
+++ labPOO/src/javaFullCource/Hello.java
@@ -0,0 +1,23 @@
+package javaFullCource;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Este print adiciona adiciona uma nova linha no final.");
+		System.out.print("Este print não adiciona nova linha.");
+		System.out.print("Mas também dá pra usar \\n para quebrar linha.\n");
+		System.out.println("Que dá certo também.");
+		System.out.println("\t este cara \\t adiciona tab.");
+		System.out.println("Se vc fizer 'sysout' e ctrl + space, o eclipse faz o autocomplete do print.");
+		// é assim que a gente faz pra fazer um comentário.
+		
+		/*
+		 * e assim
+		 * a gente 
+		 * faz um 
+		 * comentário de
+		 * múltiplas linhas
+		 */
+	}
+
+}
---
+++ labPOO/src/javaFullCource/Variables.java
@@ -0,0 +1,23 @@
+package javaFullCource;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int x = 123;
+		long y = 3217317298379217L;
+		float z = 3.14f;
+		double w = 3.141592653589793238462643383279502884;
+		boolean h = true;
+		char symbol = '@';
+		String name = "Cloves Paiva";
+		
+		System.out.println("Este é um inteiro curto: " + x);
+		System.out.println("Este é um inteiro longo: " + y);
+		System.out.println("Este é uma decimal curto: " + z);
+		System.out.println("Este é um decimal longo: " + w);
+		System.out.println("Este é um booleano: " + h);
+		System.out.println("Este é um caracter: " + symbol);
+		System.out.println("Este é uma string: " + name);
+	}
+
+}
